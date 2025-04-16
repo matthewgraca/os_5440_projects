c8e3bf5b9d1088cd0311e23402e295e95a67b899
Matthew Graca <matthew.graca@gmail.com>
2025-04-16 15:37:31 -0700

    added balls sequentially falling

--- 5_balls/ThreadingBall.java
+++ 5_balls/ThreadingBall.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import javax.swing.*;
+import java.util.concurrent.*;
 
 class MyBall extends Thread{
   int x;
@@ -10,8 +11,17 @@ class MyBall extends Thread{
   Color c;
   String str, substr;
   boolean goingDown;
+  Semaphore pre_semaphore, post_semaphore;
 
-  public MyBall(int xx, int yy, int sleepTime, MyBallWindow ssbb, Color cc){
+  public MyBall(
+      int xx, 
+      int yy, 
+      int sleepTime, 
+      MyBallWindow ssbb, 
+      Color cc,
+      Semaphore pre_semaphore,
+      Semaphore post_semaphore
+  ){
     str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     str_index = 0;
     substr = "A";
@@ -21,27 +31,33 @@ class MyBall extends Thread{
     sleeptime = sleepTime;
     mBw = ssbb;
     c = cc;
+    this.pre_semaphore = pre_semaphore;
+    this.post_semaphore = post_semaphore;
   }
 
   public void run(){
     while (true){
-      mBw.repaint();
-      substr = str.substring(str_index, str_index+1);
-      str_index++;
-      if (str_index >= str.length()) str_index = 0;
-
-      if (goingDown){
-        y += 10;
-        if (y > 550) goingDown = false;
-      }
-      else{
-        y -= 10;
-        if (y <= 0) goingDown = true;
-      }
-
       try{
+        pre_semaphore.acquire();
         Thread.sleep(sleeptime);
-      } catch (Exception e){}
+
+        mBw.repaint();
+        substr = str.substring(str_index, str_index+1);
+        str_index++;
+        if (str_index >= str.length()) str_index = 0;
+
+        if (goingDown){
+          y += 10;
+          if (y > 550) goingDown = false;
+        }
+        else{
+          y -= 10;
+          if (y <= 0) goingDown = true;
+        }
+      } catch (Exception e) {}
+      finally {
+        post_semaphore.release();
+      }
     }
   }
 
@@ -65,14 +81,30 @@ class MyBallWindow extends JFrame{
   MyBall myBall[];
 
   public MyBallWindow(){
+    Semaphore sem1 = new Semaphore(1);
+    Semaphore sem2 = new Semaphore(0);
+    Semaphore sem3 = new Semaphore(0);
+    Semaphore sem4 = new Semaphore(0);
+    Semaphore sem5 = new Semaphore(0);
+    Semaphore sem6 = new Semaphore(0);
+    int sleepTime = 500;  // any faster, and the balls sometimes drops together
     myBall = new MyBall[6];
 
-    myBall[0] = new MyBall(100, 0, 300, this, Color.red);
-    myBall[1] = new MyBall(200, 0, 300, this, Color.blue);
-    myBall[2] = new MyBall(300, 0, 300, this, Color.yellow); 
-    myBall[3] = new MyBall(400, 0, 300, this, Color.green);
-    myBall[4] = new MyBall(500, 0, 300, this, Color.orange);
-    myBall[5] = new MyBall(600, 0, 300, this, Color.pink);
+    /*
+    myBall[0] = new MyBall(100, 0, 300, this, Color.red, sem6, sem1);
+    myBall[1] = new MyBall(200, 0, 300, this, Color.blue, sem3, sem4);
+    myBall[2] = new MyBall(300, 0, 300, this, Color.yellow, sem4, sem5); 
+    myBall[3] = new MyBall(400, 0, 300, this, Color.green, sem2, sem3);
+    myBall[4] = new MyBall(500, 0, 300, this, Color.orange, sem5, sem6);
+    myBall[5] = new MyBall(600, 0, 300, this, Color.pink, sem1, sem2);
+    */
+
+    myBall[0] = new MyBall(100, 0, sleepTime, this, Color.pink, sem1, sem2);
+    myBall[1] = new MyBall(200, 0, sleepTime, this, Color.green, sem2, sem3);
+    myBall[2] = new MyBall(300, 0, sleepTime, this, Color.blue, sem3, sem4); 
+    myBall[3] = new MyBall(400, 0, sleepTime, this, Color.yellow, sem4, sem5);
+    myBall[4] = new MyBall(500, 0, sleepTime, this, Color.orange, sem5, sem6);
+    myBall[5] = new MyBall(600, 0, sleepTime, this, Color.red, sem6, sem1);
 
     for (int i = 0; i < 6; i++){ 
       myBall[i].start();   
