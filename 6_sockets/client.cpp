cc89d542f8b59e7338dddfb7a82e7e0f7b785738
Matthew Graca <matthew.graca@gmail.com>
2025-04-28 23:19:17 -0700

    cleaned up

--- 6_sockets/client.cpp
+++ 6_sockets/client.cpp
@@ -3,71 +3,77 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-
 #include <iostream>
+
+#define CLIENT_SOCKET 12340
+#define SERVER_SOCKET 12341
+#define BUFF_LEN 200
 
 using namespace std;
 
-int main()
-{
-    int pid = fork();
-    if (pid == 0){
-      int sockfd = 0;
+int main(){
+  int pid = fork();
+  // child: client, reads  
+  if (pid == 0){
+    // define client socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-      sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in servaddr;
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(CLIENT_SOCKET);
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-      struct sockaddr_in servaddr;
-      servaddr.sin_family = AF_INET;
-      servaddr.sin_port = htons(12340);
-      servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-      cout << "Connecting..." << endl;
-      while (connect(sockfd,(struct sockaddr *)&servaddr, sizeof(struct sockaddr)) == -1){
-        cout << "..." << endl;
-        sleep(1);
-      }
-      cout << "Connected!" << endl;
-
-      int msglen;
-      char buf[200];
-      msglen= recv(sockfd,buf, 200, 0);
-      while (msglen > 0)
-      {
-         cout << buf << endl;
-         msglen= recv(sockfd,buf, 200, 0);
-      }
-      close(sockfd);
-
+    // connect to client socket
+    cout << "Connecting to client socket..." << endl;
+    while (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(struct sockaddr)) == -1){
+      cout << "..." << endl;
+      sleep(1);
     }
-    else{
-      int sockfd = 0, newsockfd = 0, BACKLOG = 5;
+    cout << "Connected! You can now read messages sent by the server." << endl;
 
-      sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-      struct sockaddr_in my_addr, their_addr;
-      my_addr.sin_family = AF_INET;
-      my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-      my_addr.sin_port = htons(12341);
-
-      bind(sockfd,(struct sockaddr*)&my_addr, sizeof(my_addr));
-
-      listen(sockfd, BACKLOG);
-
-      while(1)
-      {
-        socklen_t sin_size = sizeof(struct sockaddr_in);
-        newsockfd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-        char inputbuf[200];
-
-        cin >> inputbuf;
-        while (sizeof(inputbuf) > 0)
-        {
-         send(newsockfd, inputbuf, 200, 0);
-         cin >> inputbuf;
-        }
-        close(newsockfd);
-      }
-      close(sockfd);
+    // read messages
+    char buf[BUFF_LEN];
+    int msglen = 1;
+    while (msglen > 0){
+      msglen = recv(sockfd, buf, BUFF_LEN, 0);
+      cout << buf << endl;
     }
-    return 0;
+
+    // close client socket once finished
+    close(sockfd);
+  }
+  else{
+    // create server socket
+    int sockfd = 0, newsockfd = 0, BACKLOG = 5;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in my_addr, their_addr;
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    my_addr.sin_port = htons(SERVER_SOCKET);
+
+    // bind and listen
+    bind(sockfd,(struct sockaddr*)&my_addr, sizeof(my_addr));
+    listen(sockfd, BACKLOG);
+
+    // accept connection from client socket
+    socklen_t sin_size = sizeof(struct sockaddr_in);
+    newsockfd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+
+    // write data
+    char inputbuf[BUFF_LEN];
+    while (true){
+      cin >> inputbuf;
+      send(newsockfd, inputbuf, BUFF_LEN, 0); 
+    }
+
+    // close connection to client socket
+    close(newsockfd);
+
+    // close server socket
+    close(sockfd);
+  }
+
+  return 0;
 }
