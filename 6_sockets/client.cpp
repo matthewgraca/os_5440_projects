5e40ac28f07d1f93cef60033cbf529cf498cd071
Matthew Graca <matthew.graca@gmail.com>
2025-04-30 17:57:51 -0700

    tried improving the exit, process still running in background?

--- 6_sockets/client.cpp
+++ 6_sockets/client.cpp
@@ -12,20 +12,26 @@
 using namespace std;
 
 int main(){
-  int pid = fork();
+  // define client socket
+  int client_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in servaddr;
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(CLIENT_SOCKET);
+  servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+  // create server socket
+  int server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in my_addr, their_addr;
+  my_addr.sin_family = AF_INET;
+  my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  my_addr.sin_port = htons(SERVER_SOCKET);
+
   // child: client, reads  
+  int pid = fork();
   if (pid == 0){
-    // define client socket
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    struct sockaddr_in servaddr;
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(CLIENT_SOCKET);
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
     // connect to client socket
     cout << "Connecting to client socket..." << endl;
-    while (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(struct sockaddr)) == -1){
+    while (connect(client_sockfd, (struct sockaddr *)&servaddr, sizeof(struct sockaddr)) == -1){
       cout << "..." << endl;
       sleep(1);
     }
@@ -35,32 +41,24 @@ int main(){
     char buf[BUFF_LEN];
     int msglen = 1;
     while (msglen > 0){
-      msglen = recv(sockfd, buf, BUFF_LEN, 0);
+      msglen = recv(client_sockfd, buf, BUFF_LEN, 0);
       string msg = (msglen <= 0) ? "Peer has left, exiting..." : "B: " + string(buf);
       cout << msg << endl;
     }
 
-    // close client socket once finished
-    close(sockfd);
+    // notify sockets to shutdown
+    shutdown(client_sockfd, SHUT_RDWR);
+    shutdown(server_sockfd, SHUT_RDWR);
   }
   else{
-    // create server socket
-    int sockfd = 0, newsockfd = 0, BACKLOG = 5;
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    struct sockaddr_in my_addr, their_addr;
-    my_addr.sin_family = AF_INET;
-    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    my_addr.sin_port = htons(SERVER_SOCKET);
-
+    int newsockfd = 0, BACKLOG = 5;
     // bind and listen
-    bind(sockfd,(struct sockaddr*)&my_addr, sizeof(my_addr));
-    listen(sockfd, BACKLOG);
+    bind(server_sockfd,(struct sockaddr*)&my_addr, sizeof(my_addr));
+    listen(server_sockfd, BACKLOG);
 
     // accept connection from client socket
     socklen_t sin_size = sizeof(struct sockaddr_in);
-    newsockfd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+    newsockfd = accept(server_sockfd, (struct sockaddr *)&their_addr, &sin_size);
 
     cout << "Connection accepted from client. You can now send messages. Type \"/q\" to exit." << endl;
 
@@ -72,12 +70,14 @@ int main(){
       retval = (string(inputbuf) == "/q") ? -1 : send(newsockfd, inputbuf, BUFF_LEN, 0); 
     }
 
-    // close connection to client socket
+    // close sockets
+    shutdown(server_sockfd, SHUT_RDWR);
+    shutdown(client_sockfd, SHUT_RDWR);
     close(newsockfd);
-
-    // close server socket
-    close(sockfd);
   }
 
+  // close sockets
+  close(server_sockfd);
+  close(client_sockfd);
   return 0;
 }
